da8cd25c85aacded02ae1a616558bbf8475c7776
Akshaya Jonnalagadda <ajonnalagadda@ezesoft.com>
2021-04-21 15:33:06 +0530

    Add project files.

---
+++ Publisher/Publisher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using STAN.Client;
+
+namespace Publisher
+{
+	class Publisher
+	{
+		static void Main(string[] args)
+		{
+			string clientId = $"producer-{Guid.NewGuid().ToString()}";
+
+			var cf = new StanConnectionFactory();
+			StanOptions options = StanOptions.GetDefaultOptions();
+			options.NatsURL = "nats://localhost:4222";
+
+			using (var c = cf.CreateConnection("test-cluster", clientId, options))
+			{
+				do
+				{
+					var opts = StanSubscriptionOptions.GetDefaultOptions();
+					opts.StartWithLastReceived();
+					var s = c.Subscribe("nats.streaming.demo.client", opts, (obj, arguments) =>
+					{
+						string message = Encoding.UTF8.GetString(arguments.Message.Data);
+						Console.WriteLine(message);
+					});
+
+					c.Publish("nats.streaming.demo", Encoding.UTF8.GetBytes("Published from the publisher"));
+					Thread.Sleep(10000);
+
+				} while (true);
+
+			}
+
+		}
+	}
+}
---
+++ Subscriber/Subscriber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NATS.Client.Rx.Ops;
+using STAN.Client;
+using STAN.Client.Rx;
+
+namespace Subscriber
+{
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: consumer <clientid>");
+                return;
+            }
+
+            string clientId = args[0];
+
+            var cf = new StanConnectionFactory();
+
+            StanOptions options = StanOptions.GetDefaultOptions();
+            options.NatsURL = "nats://localhost:4222";
+            using (var c = cf.CreateConnection("test-cluster", clientId, options))
+            { 
+                var opts = StanSubscriptionOptions.GetDefaultOptions();
+                opts.StartWithLastReceived();
+                Task.Run(() =>
+                {
+                    var s = c.Subscribe("nats.streaming.demo", opts, (obj, arguments) =>
+                    {
+                        string message = Encoding.UTF8.GetString(arguments.Message.Data);
+                        Console.WriteLine(message);
+                        c.Publish("nats.streaming.demo.client", Encoding.UTF8.GetBytes("publishing from consumer " + clientId));
+                    });
+                });
+                Console.WriteLine($"Consumer with client id '{clientId}' started. Press any key to quit...");
+                Console.ReadKey(true);
+
+                //s.Unsubscribe();
+                c.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
